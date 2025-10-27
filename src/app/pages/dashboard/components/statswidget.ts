7e611f3288ff64573ba35a182215e95c3326ba3f
Elianyi Otaño <eotano@arsreservas.com>
2025-10-26 20:53:05 -0400

    feat: change color of cards

--- src/app/pages/dashboard/components/statswidget.ts
+++ src/app/pages/dashboard/components/statswidget.ts
@@ -12,8 +12,8 @@ import { CommonModule } from '@angular/common';
                         <span class="block text-muted-color font-medium mb-4">Orders</span>
                         <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152</div>
                     </div>
-                    <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-shopping-cart text-blue-500 text-xl!"></i>
+                    <div class="flex items-center justify-center rounded-border" style="width: 2.5rem; height: 2.5rem; background-color: #B3D4E5;">
+                        <i class="pi pi-shopping-cart" style="color: #00558C; font-size: 1.25rem;"></i>
                     </div>
                 </div>
                 <span class="text-primary font-medium">24 new </span>
@@ -27,8 +27,8 @@ import { CommonModule } from '@angular/common';
                         <span class="block text-muted-color font-medium mb-4">Revenue</span>
                         <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">$2.100</div>
                     </div>
-                    <div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-dollar text-orange-500 text-xl!"></i>
+                    <div class="flex items-center justify-center rounded-border" style="width: 2.5rem; height: 2.5rem; background-color: #FFE2B3;">
+                        <i class="pi pi-users" style="color: #FF9E1B; font-size: 1.25rem;"></i>
                     </div>
                 </div>
                 <span class="text-primary font-medium">%52+ </span>
@@ -42,8 +42,8 @@ import { CommonModule } from '@angular/common';
                         <span class="block text-muted-color font-medium mb-4">Customers</span>
                         <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">28441</div>
                     </div>
-                    <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-users text-cyan-500 text-xl!"></i>
+                    <div class="flex items-center justify-center rounded-border" style="width: 2.5rem; height: 2.5rem; background-color: #B9E8FB;">
+                        <i class="pi pi-chart-line" style="color: #00AEF0; font-size: 1.25rem;"></i>
                     </div>
                 </div>
                 <span class="text-primary font-medium">520 </span>
@@ -57,8 +57,8 @@ import { CommonModule } from '@angular/common';
                         <span class="block text-muted-color font-medium mb-4">Comments</span>
                         <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152 Unread</div>
                     </div>
-                    <div class="flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-comment text-purple-500 text-xl!"></i>
+                    <div class="flex items-center justify-center rounded-border" style="width: 2.5rem; height: 2.5rem; background-color: #D4D5D7;">
+                        <i class="pi pi-cog" style="color: #00558C; font-size: 1.25rem;"></i>
                     </div>
                 </div>
                 <span class="text-primary font-medium">85 </span>
