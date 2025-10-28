3b99784f1587ac6d43a14f7746ba8bcd1e101d9e
Elianyi Otaño <eotano@arsreservas.com>
2025-10-28 16:18:52 -0400

    feat(dev): set colors

--- src/app/shared/helper/avatar_helper.ts
+++ src/app/shared/helper/avatar_helper.ts
@@ -1,48 +1,48 @@
 export class AvatarGenerator {
-  /**
-   * Genera una imagen de perfil con iniciales y un fondo de color consistente basado en el nombre.
-   * @param name El nombre de la persona.
-   * @param surname El apellido de la persona.
-   * @returns Un string con la URL de la imagen generada en formato base64.
-   */
-  static generateAvatar(name: string, surname: string): string {
-    const canvas = document.createElement('canvas');
-    canvas.width = 60;
-    canvas.height = 60;
-    const ctx = canvas.getContext('2d');
-    if (!ctx) {
-      throw new Error('No se pudo obtener el contexto del canvas.');
+    /**
+     * Genera una imagen de perfil con iniciales y un fondo de color consistente basado en el nombre.
+     * @param name El nombre de la persona.
+     * @param surname El apellido de la persona.
+     * @returns Un string con la URL de la imagen generada en formato base64.
+     */
+    static generateAvatar(name: string, surname: string): string {
+        const canvas = document.createElement('canvas');
+        canvas.width = 60;
+        canvas.height = 60;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            throw new Error('No se pudo obtener el contexto del canvas.');
+        }
+
+        // Colores predefinidos
+        const colors = ['#063452', '#00aef0', '#ff9e1b', '#63666a', '#a7a8a9'];
+        const colorIndex = AvatarGenerator.hashCode(name + surname) % colors.length;
+        const consistentColor = colors[Math.abs(colorIndex)];
+
+        ctx.fillStyle = consistentColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Iniciales
+        const initials = `${name.charAt(0).toUpperCase()}${surname.charAt(0).toUpperCase()}`;
+        ctx.font = '600 28px "Poppins", sans-serif';
+        ctx.fillStyle = '#FFFFFF';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(initials, canvas.width / 2, canvas.height / 2);
+
+        return canvas.toDataURL('image/png');
     }
 
-    // Colores predefinidos
-    const colors = ['#f4768b', '#5fc2dd', '#203e7a', '#ffa726', '#42a5f5'];
-    const colorIndex = AvatarGenerator.hashCode(name + surname) % colors.length;
-    const consistentColor = colors[Math.abs(colorIndex)];
-
-    ctx.fillStyle = consistentColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Iniciales
-    const initials = `${name.charAt(0).toUpperCase()}${surname.charAt(0).toUpperCase()}`;
-    ctx.font = '600 28px "Poppins", sans-serif';
-    ctx.fillStyle = '#FFFFFF';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(initials, canvas.width / 2, canvas.height / 2);
-
-    return canvas.toDataURL('image/png');
-  }
-
-  /**
-   * Genera un hash numérico a partir de una cadena de texto.
-   * @param str La cadena de texto.
-   * @returns Un número hash.
-   */
-  private static hashCode(str: string): number {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    /**
+     * Genera un hash numérico a partir de una cadena de texto.
+     * @param str La cadena de texto.
+     * @returns Un número hash.
+     */
+    private static hashCode(str: string): number {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        return hash;
     }
-    return hash;
-  }
 }
